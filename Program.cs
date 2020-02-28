d2194b7e95474b1a064f29484e1b467fae17410f
Brendan Krekeler <36323683+okrek6@users.noreply.github.com>
2020-02-27 23:10:45 -0600

    First Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DocumentMerger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
