f9630a3118e21f1566f7891557c15be1488c460e
Brendan Krekeler <36323683+okrek6@users.noreply.github.com>
2020-02-28 01:53:00 -0600

    This is not working correctly

--- Program.cs
+++ Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
-
+using System.IO;
 namespace DocumentMerger
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length < 3)
+            {
+                Console.WriteLine("DocumentMerger2 <input_file_1> <input_file_2> <input_file_n> <output_file>");
+                Console.WriteLine("Give some text files to be MERGED. Make sure to include the name of the file that will include the merged files.");
+                Console.WriteLine("No less than two files can be provided");
+            }
+
+            string[] userFiles = new string[args.Length-1];
+            Array.Copy(args, 0, userFiles, 0, args.Length - 1);
+
+            var newFile = args[args.Length - 1];
+            StreamWriter write = null;
+            try 
+            {
+                write = new StreamWriter(newFile);
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("Error opening to write to {0}: {1}", newFile, error.Message);
+
+
+                Console.WriteLine("idk");
+            }
         }
     }
 }
